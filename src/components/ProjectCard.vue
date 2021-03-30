e0e87c2ec39626786af3f792533035d651a89a4a
Ram Chaudhari <ram.chaudhari@exponentia.ai>
2021-03-30 10:03:09 +0530

    changes for projects

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="w-full md:w-1/2 xl:w-1/3 mb-6"
+    :class="{
+      'text-gray-800': darkState,
+      'text-white': !darkState,
+    }"
+  >
+    <div class="mx-8 rounded-lg bg-white shadow-lg">
+      333333
+      <!-- <slot></slot> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectCard",
+  // props: {
+  //   darkState: String,
+  // },
+};
+</script>
+
+<style></style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,14 +1,80 @@
 <template>
   <div
-    class="h-screen text-center pt-24 text-2xl uppercase tracking-widest"
+    class="h-screen pt-24"
     :class="{
       'text-yellow-400': darkState,
-      'text-green-500': !darkState
+      'text-green-500': !darkState,
     }"
   >
-    PROJECTS
-    <div class="pt-24 text-xl">
-      {{ getMessage() }}
+    <div class="text-2xl text-center uppercase tracking-widest">
+      MY PROJECTS
+    </div>
+
+    <div class="flex flex-wrap justify-center w-full mt-12">
+      <div
+        class="w-full md:w-1/2 xl:w-1/3 mb-6"
+        :class="{
+          'text-gray-800': darkState,
+          'text-white': !darkState,
+        }"
+      >
+        <div class="mx-8 p-6 rounded-lg bg-white shadow-xl">
+          <img
+            src="../assets/images/chatbot.jpg"
+            class="w-full rounded-xl"
+            alt=""
+          />
+          <div>Engagely Chatbots</div>
+        </div>
+      </div>
+      <div
+        class="w-full md:w-1/2 xl:w-1/3 mb-6"
+        :class="{
+          'text-gray-800': darkState,
+          'text-white': !darkState,
+        }"
+      >
+        <div class="mx-8 p-6 rounded-lg bg-white shadow-xl">
+          <img
+            src="../assets/images/chatbot.jpg"
+            class="w-full rounded-xl"
+            alt=""
+          />
+          <div>Engagely Chatbots</div>
+        </div>
+      </div>
+      <div
+        class="w-full md:w-1/2 xl:w-1/3 mb-6"
+        :class="{
+          'text-gray-800': darkState,
+          'text-white': !darkState,
+        }"
+      >
+        <div class="mx-8 p-6 rounded-lg bg-white shadow-xl">
+          <img
+            src="../assets/images/chatbot.jpg"
+            class="w-full rounded-xl"
+            alt=""
+          />
+          <div>Engagely Chatbots</div>
+        </div>
+      </div>
+      <div
+        class="w-full md:w-1/2 xl:w-1/3 mb-6"
+        :class="{
+          'text-gray-800': darkState,
+          'text-white': !darkState,
+        }"
+      >
+        <div class="mx-8 p-6 rounded-lg bg-white shadow-xl">
+          <img
+            src="../assets/images/chatbot.jpg"
+            class="w-full rounded-xl"
+            alt=""
+          />
+          <div>Engagely Chatbots</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,16 +82,16 @@
 <script>
 export default {
   methods: {
-    getMessage: () => {
-      const arr = ["appearing shortly", "anytime here", "check back soon"];
-      return arr[Math.floor(Math.random() * arr.length)];
-    }
+    // getMessage: () => {
+    //   const arr = ["appearing shortly", "anytime here", "check back soon"];
+    //   return arr[Math.floor(Math.random() * arr.length)];
+    // }
   },
   computed: {
     darkState() {
       return this.$store.state.darkTheme;
-    }
-  }
+    },
+  },
 };
 </script>
 
